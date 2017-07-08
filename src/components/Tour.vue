2fa1a916c44769b40c7a19c894be83c2a315766a
ScottHensen <hensen.scott@gmail.com>
2017-07-07 22:12:08 -0700

    Build tour from s3 json file

--- src/components/Tour.vue
+++ src/components/Tour.vue
@@ -2,11 +2,11 @@
   <div id="app">
     <div class="jumbotron" id="summerPoster">
       <h1>{{title}}</h1>
-      <p>{{subtitle}}</p>
+      <p>{{tour.tour}}</p>
     </div>
     <br />
     <ShowSelection
-      v-for="(show, index) in shows"
+      v-for="(show, index) in tour.shows"
       v-bind:passed-show="show"
       key="index"
     />
@@ -21,41 +21,23 @@
     data() {
       return {
         title: 'Dead & Company',
-        subtitle: 'Summer 2017',
-        shows: [
-            {
-              date: "20170527",
-              day: "Saturday",
-              venue: "MGM Grand Garden Arena",
-              city: "Las Vegas",
-              state: "NV",
-              // poster: "./src/assets/posters/poster_20170527b_lasvegas.jpg"
-              poster: "https://s3-us-west-2.amazonaws.com/deadco.show/2017Summer/posters/poster_20170527b_lasvegas.jpg"
-            },
-            {
-              date: "20170528",
-              day: "Sunday",
-              venue: "Ak-Chin Pavillion",
-              city: "Phoenix",
-              state: "AZ",
-              // poster: "./src/assets/posters/poster_20170528_phoenix.jpg"
-              poster: "https://s3-us-west-2.amazonaws.com/deadco.show/2017Summer/posters/poster_20170528_phoenix.jpg"
-            },
-            {
-              date: "20170531",
-              day: "Wednesday",
-              venue: "Hollywood Bowl",
-              city: "Los Angeles",
-              state: "CA",
-              // poster: "./src/assets/posters/poster_20170531_hollywood.jpg"
-              poster: "https://s3-us-west-2.amazonaws.com/deadco.show/2017Summer/posters/poster_20170531_hollywood.jpg"
-
-            }
-        ]
+        tour: {}
+      }
+    },
+    methods: {
+      getTour() {
+           fetch(`https://s3-us-west-2.amazonaws.com/deadco.show/2017Summer/tourData.json`, {
+             method: 'GET'
+           })
+             .then(response => response.json())
+             .then(json => this.tour = json)
       }
     },
     components: {
       ShowSelection
+    },
+    mounted() {
+      this.getTour();
     }
   }
 </script>
