a6834bd9849d96aece185eafc09e0ed9398d3d92
ScottHensen <hensen.scott@gmail.com>
2017-07-22 12:04:15 -0700

    format card dates and line spacing

--- src/components/ShowSelection.vue
+++ src/components/ShowSelection.vue
@@ -6,7 +6,7 @@
           <img class="poster-thumbnail" v-bind:src="show.poster" alt="poster">
         </div>
         <div class="card-details col-md-6">
-          <h4 class="card-title">{{show.date}}</h4>
+          <h4 class="card-title show-date"><span>{{formattedShowDay}},</span><span>{{formattedShowDate}}</span></h4>
           <p  class="card-text" >{{show.venue}}</p>
           <p  class="card-text" >{{show.city}}</p>
           <p  class="card-text" >{{show.state}}</p>
@@ -17,15 +17,19 @@
 </template>
 
 <script>
+  import moment from 'moment'
   export default {
     props: ['passedShow', 'index'],
     data() {
       return {
-        show: {}
+        show: {},
+        formattedShowDate: ""
       }
     },
     created() {
       this.show = this.passedShow
+      this.formattedShowDay  = moment(this.show.date,"YYYYMMDD").format('dddd');
+      this.formattedShowDate = moment(this.show.date,"YYYYMMDD").format('MMMM Do YYYY');
     }
 }
 </script>
